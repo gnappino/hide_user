74d5c15932f9e1c8de1e64ea3c10bf0fb674058b
gnappino <gnappinox1@outlook.com>
2017-06-18 22:30:48 +0200

    Aggiunti Eventi alla griglia

--- popup.js
+++ popup.js
@@ -46,14 +46,11 @@ function Test(){
 }
 
 function CreaTabella(){
-	var clients = [
-					{ "Nome": "Prova", "Frase": "Testo"}
-			];
-
 			$("#jsGrid").jsGrid({
 					width: "100%",
-					height: "400px",
+					height: "100%",
 
+					filtering: false,
 					inserting: true,
 					editing: true,
 					sorting: false,
@@ -63,15 +60,35 @@ function CreaTabella(){
 
 					fields: [
 							{ name: "Nome", type: "text", width: 150, validate: "required" },
-							{ name: "Frase", type: "text", width: 400 },
+							{ name: "Frase", type: "text", width: 350 },
+							{ type: "control" }
 					]
 			});
 			var grid = $("#jsGrid").data("JSGrid");
 			$("#jsGrid").jsGrid("insertItem", { Nome: "John", Frase: "25" }).done(function() {
-			    console.log("insertion completed");
+					console.log("insertion completed");
 			});
 			grid.insertItem({ Nome: "Pippo", Frase: "Antonella"});
-			//console.log(grid);
+
+			$("#jsGrid").jsGrid({
+					onItemInserted: function(args) {
+							console.log("Inserimento");
+							//Leggere tutti i dati della tabella e reinserirli
+					}
+			});
+			$("#jsGrid").jsGrid({
+					onItemDeleted: function(args) {
+							console.log("Eliminazione");
+							//Leggere tutti i dati della tabella e reinserirli
+					}
+			});
+			$("#jsGrid").jsGrid({
+					onItemUpdated: function(args) {
+							console.log("Aggionramento");
+							//Leggere tutti i dati della tabella e reinserirli
+					}
+			});
+
 }
 
 function InserisciDati(){
