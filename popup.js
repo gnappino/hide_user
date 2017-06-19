b31d13d75efddbc633176b2dd5dd2544fcccaedb
gnappino <gnappinox1@outlook.com>
2017-06-19 12:26:39 +0200

    Aggiunte Completata tabella, con eventi e salvataggio e caricamento Ripristinata funzionalità hide_user
    resta da fare, la parte cambia testo

--- popup.js
+++ popup.js
@@ -1,51 +1,38 @@
 document.addEventListener('DOMContentLoaded', AggListener,false)
 var out;
+var Grid;
 function AggListener(){
 	var ck=document.getElementById("ONOFF");
 //	console.log(ck);
-	CreaTabella();
-	InserisciDati();
-	/*
+	CreaTabella(Grid);
+
 	chrome.storage.sync.get("Attivo", function(items) {
 		if (!chrome.runtime.error) {
-			//console.log(items);
 		if (items["Attivo"]=="SI"){
 			ck.checked=true;
 		}
 		}
 	});
-	ck.addEventListener("click",Test,false);
-	var ta=document.getElementById("lista");
-	ta.addEventListener("keyup",Test,false);
-	ta.addEventListener("change",Test,false);
-
-	chrome.storage.sync.get("Nomi", function(items) {
-		if (!chrome.runtime.error) {
-		ElencoNomi=items["Nomi"];
-//		var ta=document.getElementById("lista");
-		ta.value=ElencoNomi;
-		//ta.addEventListener("keypress",Test,false);
-		}
-	});
-*/
+	ck.addEventListener("click",AggiornaON_OFF,false);
 }
 
-function Test(){
-	//SeparaNomi();
-	var ta=document.getElementById("lista");
-	chrome.storage.sync.set({ "Nomi" : ta.value });
-
+function AggiornaON_OFF(){
 	var ck=document.getElementById("ONOFF");
 	if (ck.checked){
-		console.log('Acceso');
+//		console.log('Acceso');
 		chrome.storage.sync.set({ "Attivo" : "SI" });
 	} else {
 		chrome.storage.sync.set({ "Attivo" : "NO" });
-		console.log('Spento');
+//		console.log('Spento');
 	}
 }
 
-function CreaTabella(){
+function SalvaDati(dati){
+	chrome.storage.sync.set({ "Nomi_Grid" : dati });
+//	console.log(dati);
+}
+
+function CreaTabella(Grid){
 			$("#jsGrid").jsGrid({
 					width: "100%",
 					height: "100%",
@@ -65,32 +52,42 @@ function CreaTabella(){
 					]
 			});
 			var grid = $("#jsGrid").data("JSGrid");
-			$("#jsGrid").jsGrid("insertItem", { Nome: "John", Frase: "25" }).done(function() {
-					console.log("insertion completed");
-			});
-			grid.insertItem({ Nome: "Pippo", Frase: "Antonella"});
-
+			// $("#jsGrid").jsGrid("insertItem", { Nome: "John", Frase: "25" }).done(function() {
+			// 		console.log("insertion completed");
+			// });
+			// grid.insertItem({ Nome: "Pippo", Frase: "Antonella"});
+			console.log(grid.data);
 			$("#jsGrid").jsGrid({
 					onItemInserted: function(args) {
 							console.log("Inserimento");
 							//Leggere tutti i dati della tabella e reinserirli
+							SalvaDati(args.grid.data);
 					}
 			});
 			$("#jsGrid").jsGrid({
 					onItemDeleted: function(args) {
 							console.log("Eliminazione");
 							//Leggere tutti i dati della tabella e reinserirli
+							SalvaDati(args.grid.data);
 					}
 			});
 			$("#jsGrid").jsGrid({
 					onItemUpdated: function(args) {
 							console.log("Aggionramento");
 							//Leggere tutti i dati della tabella e reinserirli
+							SalvaDati(args.grid.data);
 					}
 			});
 
-}
-
-function InserisciDati(){
-
+			//Questo finisce quà per via della caratteristica peculiare di JavaScript, li mortacci sua
+			chrome.storage.sync.get("Nomi_Grid", function(items) {
+				if (!chrome.runtime.error) {
+					ElencoNomi=items["Nomi_Grid"];
+					console.log("Elenco Nomi");
+					console.log(ElencoNomi);
+					grid.data=ElencoNomi;
+					grid.refresh();
+				}
+			});
+			Grid=grid;
 }
--- test.js
+++ test.js
@@ -1,85 +1,105 @@
 var elems = document.getElementsByClassName("threadbit");
 var ElencoNomi;
+var NomiNascondi=[];
+var NomiCambiaTesto=[];
+chrome.storage.sync.get("Nomi_Grid", function(items) {
+		if (!chrome.runtime.error) {
+			//console.log(items);
+			ElencoNomi=items["Nomi_Grid"];
 
-chrome.storage.sync.get("Nomi", function(items) {
-    if (!chrome.runtime.error) {
-      //console.log(items);
-      ElencoNomi=items["Nomi"];
-      ElencoNomi=ElencoNomi.split(";");
-      //console.log(ElencoNomi);
-    }
-  });
+			//ElencoNomi=ElencoNomi.split(";");
+			//console.log(ElencoNomi);
+
+			var j,k;
+			j=0;
+			k=0;
+			for (i=0;i<ElencoNomi.length;i++){
+				if (ElencoNomi[i].Frase=="") {
+					NomiNascondi[j]=ElencoNomi[i].Nome;
+					j++;
+				}
+				else {
+					NomiCambiaTesto[k]=ElencoNomi[i];
+				}
+			}
+			console.log("Nomi da nascondere");
+			console.log(NomiNascondi);
+			console.log("Nomi testo");
+			console.log(NomiCambiaTesto);
+		}
+	});
 
 chrome.storage.sync.get("Attivo", function(items) {
-    if (!chrome.runtime.error) {
-      //console.log(items);
-      if (items["Attivo"]=="SI"){
-        ElencoNomi.forEach(Nascondi);
-      }
-    }
-  });
+		if (!chrome.runtime.error) {
+			//console.log(items);
+
+			if (items["Attivo"]=="SI"){
+				NomiNascondi.forEach(Nascondi);
+			}
+		}
+	});
 
 
 function Nascondi(Nome){
-  //console.log("Nascondere:"+Nome);
-  //Nascondo i thread
-  for (i = 0; i < elems.length; i++) {
-    b=elems[i].getElementsByClassName('username understate');
-    c=b[0].text
-    if (c==Nome){
-      elems[i].style.display='none';
-    }
-    //console.log('Nome:'+c)
-  }
+	//console.log("Nascondere:"+Nome);
+	//Nascondo i thread
+	for (i = 0; i < elems.length; i++) {
+		b=elems[i].getElementsByClassName('username understate');
+		c=b[0].text
+		if (c==Nome){
+			elems[i].style.display='none';
+		}
+		//console.log('Nome:'+c)
+	}
 
-  //Post gia ignorati
-  var Ignorati=document.getElementsByClassName('postbitignored')
-  for (i = 0; i< Ignorati.length; i++)
-    Ignorati[i].style.display='none';
+	//Post gia ignorati
+	var Ignorati=document.getElementsByClassName('postbitignored')
+	for (i = 0; i< Ignorati.length; i++)
+		Ignorati[i].style.display='none';
 
-  //Post normali
-  var PP=document.getElementsByClassName('postcontainer')
-  for (i = 0; i < PP.length; i++) {
-    b=PP[i].getElementsByClassName('registrati')
-    //console.log(b);
+	//Post normali
+	var PP=document.getElementsByClassName('postcontainer')
+	for (i = 0; i < PP.length; i++) {
+		b=PP[i].getElementsByClassName('registrati')
+		//console.log(b);
 
-    if (b[0]==null){
-      b=PP[i].getElementsByClassName('moderatori')
-      //console.log("Moderatori:"+b);
-    }
-    if (b[0]==null){
-      b=PP[i].getElementsByClassName('bannati')
-      //console.log("Bannati:"+b);
-    }
-    if (b[0]==null){
-      b=PP[i].getElementsByClassName('admin')
-      //console.log("Bannati:"+b);
-    }
-    if (b[0]!=null){
-      c=b[0].textContent
-      if (c==Nome){
-        //console.log('bingo');
-        PP[i].style.display='none';
-      }
-    }
+		if (b[0]==null){
+			b=PP[i].getElementsByClassName('moderatori')
+			//console.log("Moderatori:"+b);
+		}
+		if (b[0]==null){
+			b=PP[i].getElementsByClassName('bannati')
+			//console.log("Bannati:"+b);
+		}
+		if (b[0]==null){
+			b=PP[i].getElementsByClassName('admin')
+			//console.log("Bannati:"+b);
+		}
+		if (b[0]!=null){
+			c=b[0].textContent
+			if (c==Nome){
+				//console.log('bingo');
+				PP[i].style.display='none';
+			}
+		}
 
-    //Elimino i quote
-    //console.log('Posizione:'+i);
-    Quote=PP[i].getElementsByClassName('bbcode_quote');
-    //console.log(Quote);
-    //Il for serve per i multiquote
-    for (j=0;j<Quote.length;j++) {
-      if (Quote[j]!=null) {
-        PB=Quote[j].getElementsByClassName('bbcode_postedby');
-        //console.log(PB);
-        //Prende pure i quote della firma, che possono essere senza nome
-        if (PB[0]!=null){
-          x=PB[0].getElementsByTagName("strong");
-          if (x[0].innerHTML==Nome){
-            Quote[j].style.display='none';
-          }
-        }
-      }
-    }
-  }
+		//Elimino i quote
+		//console.log('Posizione:'+i);
+		Quote=PP[i].getElementsByClassName('bbcode_quote');
+		//console.log(Quote);
+		//Il for serve per i multiquote
+		for (j=0;j<Quote.length;j++) {
+			if (Quote[j]!=null) {
+				PB=Quote[j].getElementsByClassName('bbcode_postedby');
+				//console.log(PB);
+				//Prende pure i quote della firma, che possono essere senza nome
+				if (PB[0]!=null){
+					x=PB[0].getElementsByTagName("strong");
+					if (x[0].innerHTML==Nome){
+						Quote[j].style.display='none';
+					}
+				}
+			}
+		}
+	}
 }
