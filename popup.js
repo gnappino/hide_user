fd22962327c9bf218acb817031e3955d8679acbb
gnappino <gnappinox1@outlook.com>
2017-06-19 17:57:03 +0200

    Aggiunta Importazione dati da versione vecchia

--- popup.js
+++ popup.js
@@ -3,9 +3,8 @@ var out;
 var Grid;
 function AggListener(){
 	var ck=document.getElementById("ONOFF");
-//	console.log(ck);
-	CreaTabella(Grid);
 
+	$.when(CreaTabella()).done(Aggiornamento());
 	chrome.storage.sync.get("Attivo", function(items) {
 		if (!chrome.runtime.error) {
 		if (items["Attivo"]=="SI"){
@@ -19,20 +18,17 @@ function AggListener(){
 function AggiornaON_OFF(){
 	var ck=document.getElementById("ONOFF");
 	if (ck.checked){
-//		console.log('Acceso');
 		chrome.storage.sync.set({ "Attivo" : "SI" });
 	} else {
 		chrome.storage.sync.set({ "Attivo" : "NO" });
-//		console.log('Spento');
 	}
 }
 
 function SalvaDati(dati){
 	chrome.storage.sync.set({ "Nomi_Grid" : dati });
-//	console.log(dati);
 }
 
-function CreaTabella(Grid){
+function CreaTabella(){
 			$("#jsGrid").jsGrid({
 					width: "100%",
 					height: "100%",
@@ -51,12 +47,12 @@ function CreaTabella(Grid){
 							{ type: "control" }
 					]
 			});
-			var grid = $("#jsGrid").data("JSGrid");
+			Grid = $("#jsGrid").data("JSGrid");
 			// $("#jsGrid").jsGrid("insertItem", { Nome: "John", Frase: "25" }).done(function() {
 			// 		console.log("insertion completed");
 			// });
-			// grid.insertItem({ Nome: "Pippo", Frase: "Antonella"});
-			console.log(grid.data);
+			// Grid.insertItem({ Nome: "Pippo", Frase: "Antonella"});
+			console.log(Grid.data);
 			$("#jsGrid").jsGrid({
 					onItemInserted: function(args) {
 							console.log("Inserimento");
@@ -85,9 +81,39 @@ function CreaTabella(Grid){
 					ElencoNomi=items["Nomi_Grid"];
 					console.log("Elenco Nomi");
 					console.log(ElencoNomi);
-					grid.data=ElencoNomi;
-					grid.refresh();
+					Grid.data=ElencoNomi;
+					Grid.refresh();
 				}
 			});
-			Grid=grid;
+			console.log("Tabella creata")
+}
+
+function Aggiornamento(){
+	console.log("Aggiornamento");
+	chrome.storage.sync.get("Ver", function(items) {
+			if (!chrome.runtime.error) {
+				Vers=items["Ver"];
+				console.log("1");
+				AggiungiNomi(Vers);
+			}
+		});
+}
+
+function AggiungiNomi(Vers){
+	if (Vers===undefined ){
+		console.log("2");
+		chrome.storage.sync.get("Nomi", function(items) {
+				if (!chrome.runtime.error) {
+					console.log("3");
+					ElencoNomi=items["Nomi"];
+					ElencoNomi=ElencoNomi.split(";");
+					console.log("ElencoNomi");
+					console.log(ElencoNomi)
+					console.log(Grid);
+					for (i=0;i<ElencoNomi.length;i++)
+						Grid.insertItem({Nome:ElencoNomi[i], Frase:""});
+					chrome.storage.sync.set({ "Ver" : "1" });
+				}
+			});
+		}
 }
